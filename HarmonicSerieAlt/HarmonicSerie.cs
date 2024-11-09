435acd184d0f7f14eb0f636e4d1c0863e1a61f8d
LordSexyWolf <joshuasalinashernandez@gmail.com>
2024-11-09 12:30:47 -0600

    Add project files.

---
+++ HarmonicSerieAlt/HarmonicSerie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HarmonicSerieAlt
+{
+    public class HarmonicSerie
+    {
+        public static double AlternativeHarmonicSerie(int t)
+        {
+            double r = 0.0;
+
+            for(int n = 0; n <= t; n++)
+            {
+                r += Math.Pow(-1, n + 1) / n;
+            }
+            return r;
+        }
+    }
+}
---
+++ HarmonicSerieAlt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HarmonicSerieAlt;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int t = 1000;
+        double sum = Harmonm
+    }
+}
